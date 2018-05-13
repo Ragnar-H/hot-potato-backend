7f0a1054b80f3741a61ef59ea1ef168461d3a31d
Ragnar-H <ragnarhardarson90@gmail.com>
2018-05-13 21:46:14 +0200

    Tossing potatoes around

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Mostly just copied from
 # https://medium.com/google-cloud/firebase-developing-a-web-service-with-admin-sdk-flask-and-google-cloud-6fb97eb38b80
 import random
+from datetime import datetime, timedelta
 from flask import Flask, request, jsonify, abort
 import firebase_admin
 from firebase_admin import db
@@ -42,15 +43,18 @@ def get_random_user():
 
 @APP.route("/potato", methods=["POST"])
 def handle_potato():
-    potato = {"foo": "bar"}
-    potato = POTATO.push(potato)
-
+    potato, potato_id = _get_first_potato()
+    if potato_id:
+        potato = _new_holder(potato)
+        POTATO.child(potato_id).update(potato)
+    else:
+        POTATO.push(potato)
     return jsonify(potato)
 
 
-@APP.route("/potato/<potato_id>")
-def get_potato(potato_id):
-    potato = _ensure_potato(potato_id)
+@APP.route("/potato", methods=["GET"])
+def get_potato():
+    potato = _get_first_potato()
     return jsonify(potato)
 
 
@@ -61,11 +65,43 @@ def _ensure_potato(potato_id):
     return potato
 
 
+def _get_first_potato():
+    potato = POTATO.order_by_key().limit_to_first(1).get()
+    if not potato:
+        return _create_potato()
+    potato_id = list(potato.keys())[0]
+    peeled_potato = potato[potato_id]
+    return peeled_potato, potato_id
+
+
+def _create_potato():
+    catcher = _get_random_user()
+    potato = {
+        "explosion": _get_random_explosion_time(),
+        "holder": {"id": catcher["user_id"], "username": catcher["username"]},
+    }
+    return potato
+
+
 def _get_random_user():
     users = USERS.get()
     random_user_id = random.choice(list(users))
     user = users[random_user_id]
+    user["user_id"] = random_user_id
     return user
+
+
+def _new_holder(potato):
+    catcher = _get_random_user()
+    potato["holder"] = {"id": catcher["user_id"], "username": catcher["username"]}
+    return potato
+
+
+def _get_random_explosion_time():
+    now = datetime.now()
+    time_to_explosion = random.randrange(5, 10)
+    explosion_time_timestamp = (now + timedelta(seconds=time_to_explosion)).isoformat()
+    return explosion_time_timestamp
 
 
 if __name__ == "__main__":
